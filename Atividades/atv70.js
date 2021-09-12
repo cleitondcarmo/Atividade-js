cc5bbcf3d77667aa6c40ef19894fc373d56272a1
cleitondcarmo <81137205+cleitondcarmo@users.noreply.github.com>
2021-09-11 22:46:13 -0300

    att

--- Atividades/atv70.js
+++ Atividades/atv70.js
@@ -6,7 +6,7 @@ os atributos nome e media, que indica o aluno que teve o melhor desempenho nas n
 const soma = array => array.reduce((acumulador, atual) => acumulador + atual, 0)
 const media = array => soma(array) / array.length
 
-function recerberMelhorEstudante(estudantes) {
+function MelhorEstudante(estudantes) {
     const estudantesComMedias = Object.entries(estudantes).map( estudante => {
         const chave = 0,
         valor = 1
@@ -17,7 +17,7 @@ function recerberMelhorEstudante(estudantes) {
     console.log(melhorEstudante)
 }
 
-recerberMelhorEstudante({
+MelhorEstudante({
     Joao: [8, 7.6, 8.9, 6], 
     Mariana: [9, 6.6, 7.9, 8], 
     Carla: [7, 7, 8, 9] 
